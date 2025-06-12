1777febbecca8fa08c4f2309600978cc75d3e3db
Benjamin Humpherys <brhumphe@users.noreply.github.com>
2025-06-12 00:16:55 -0700

    Make api requests in parallel

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-import json
+import asyncio
+from contextlib import asynccontextmanager
 
 import httpx
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from map_tools import find_capturable_bases
 
-app = FastAPI()
+
+async def fetch_json(client, url, params=None):
+    response = await client.get(url, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # customize settings
+    limits = httpx.Limits(max_keepalive_connections=5, max_connections=10)
+    timeout = httpx.Timeout(5.0, read=15.0)  # 15s timeout on read. 5s timeout elsewhere.
+
+    # Initialize the Client on startup and add it to the state
+    async with httpx.AsyncClient(limits=limits, timeout=timeout) as client:
+        yield {'client': client}
+        # The Client closes on shutdown
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
@@ -14,31 +34,21 @@ async def root():
 
 
 @app.get("/capturable-bases/")
-async def capturable_bases(world_id: int, zone_id: int, faction_id: int):
-    # async with httpx.AsyncClient() as client:
-    #     response = await client.get(
-    #         f"https://census.daybreakgames.com/get/ps2:v2/zone?zone_id={zone_id}&c:join=map_region^list:1^inject_at:regions^hide:zone_id(map_hex^list:1^inject_at:hexes^hide:zone_id%27map_region_id)&c:lang=en", )
-    #     response.raise_for_status()
-    #     zone_list = response.json()
-    async with httpx.AsyncClient() as client:
-        response = await client.get(f"https://census.lithafalcon.cc/get/ps2/map_state",
-                                    params={"world_id": world_id, "zone_id": zone_id, "c:censusJSON": False,
-                                            "c:show": "world_id,zone_id,timestamp,owning_faction_id,map_region_id"
-                                            })
-        response.raise_for_status()
-        map_state = response.json()
-    async with httpx.AsyncClient() as client:
-        response = await client.get(
-            f"https://census.lithafalcon.cc/get/ps2/map_region",
-            params={"zone_id": zone_id, "c:censusJSON": False, "c:show": "map_region_id,facility_id,zone_id"}
-        )
-        response.raise_for_status()
-        region_list = response.json()
-    async with httpx.AsyncClient() as client:
-        response = await client.get(f"https://census.lithafalcon.cc/get/ps2/facility_link",
-                                    params={"zone_id": zone_id, "c:censusJSON": False}
-                                    )
-        response.raise_for_status()
-        facility_links = response.json()
+async def capturable_bases(world_id: int, zone_id: int, faction_id: int, request: Request):
+    client = request.state.client
+    map_state_co = fetch_json(client, f"https://census.lithafalcon.cc/get/ps2/map_state",
+                              params={"world_id": world_id, "zone_id": zone_id, "c:censusJSON": False,
+                                      "c:show": "world_id,zone_id,timestamp,owning_faction_id,map_region_id"})
+
+    region_list_co = fetch_json(client, f"https://census.lithafalcon.cc/get/ps2/map_region",
+                                params={"zone_id": zone_id, "c:censusJSON": False,
+                                        "c:show": "map_region_id,facility_id,zone_id"})
+
+    facility_links_co = fetch_json(client, f"https://census.lithafalcon.cc/get/ps2/facility_link",
+                                   params={"zone_id": zone_id, "c:censusJSON": False})
+
+    map_state, region_list, facility_links = await asyncio.gather(map_state_co,
+                                                                  region_list_co,
+                                                                  facility_links_co)
 
     return find_capturable_bases(faction_id, facility_links, map_state, region_list)
