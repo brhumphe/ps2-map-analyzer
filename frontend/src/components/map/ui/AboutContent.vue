b397826e88739a4911e27496d3930775c22e26dc
Benjamin Humpherys <brhumphe@users.noreply.github.com>
2025-08-20 13:59:20 -0500

    Extract and modularize About content into reusable component.

---
+++ frontend/src/components/map/ui/AboutContent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2 class="mb-4">What is it?</h2>
+    <p class="mb-4">
+      A tool for understanding the strategic situation going on right now in the
+      smash hit MMO-FPS known as Planetside 2! It provides a real-time
+      visualization of what is going on RIGHT NOW, and the importance of
+      different bases. The goal is to help YOU understand the game at a
+      strategic level and choose your fights accordingly.
+    </p>
+    <p class="mb-4">
+      The goal is to help players "read the map", a crucial skill for squad and
+      platoon leaders.
+    </p>
+
+    <h2 class="mb-4">How do I use it?</h2>
+    <p class="mb-4">
+      Select the server and continent you are playing on, then the faction who's
+      strategic POV you are interested in. The map will update to highlight the
+      bases your faction can currently attack or defend, and in the future even
+      more information that will help you make informed decisions.
+    </p>
+  </div>
+</template>
